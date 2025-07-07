3ef37ffb40f964aa7f4f39b15d03cfb95174c049
slangeli2005 <slangeli@montgomerycollege.edu>
2025-07-07 02:43:32 -0400

    Create GradeBook.java

---
+++ CMSC203_Lab3/GradeBook.java
@@ -0,0 +1,107 @@
+import java.util.ArrayList;
+
+public class GradeBook
+{
+   private double[] scores;
+   private int scoresSize;
+
+   /**
+      Constructs a gradebook with no scores and a given capacity.
+      @capacity the maximum number of scores in this gradebook
+   */
+   public GradeBook(int capacity)
+   {
+      scores = new double[capacity];
+      scoresSize = 0;
+   }
+
+   /**
+      Adds a score to this gradebook.
+      @param score the score to add
+      @return true if the score was added, false if the gradebook is full
+   */
+   public boolean addScore(double score)
+   {
+      if (scoresSize < scores.length)
+      {
+         scores[scoresSize] = score;
+         scoresSize++;
+         return true;
+      }
+      else
+         return false;      
+   }
+
+   /**
+      Computes the sum of the scores in this gradebook.
+      @return the sum of the scores
+   */
+   public double sum()
+   {
+      double total = 0;
+      for (int i = 0; i < scoresSize; i++)
+      {
+         total = total + scores[i];
+      }
+      return total;
+   }
+      
+   /**
+      Gets the minimum score in this gradebook.
+      @return the minimum score, or 0 if there are no scores.
+   */
+   public double minimum()
+   {
+      if (scoresSize == 0) return 0;
+      double smallest = scores[0];
+      for (int i = 1; i < scoresSize; i++)
+      {
+         if (scores[i] < smallest)
+         {
+            smallest = scores[i];
+         }
+      }
+      return smallest;
+   }
+
+   /**
+      Gets the final score for this gradebook.
+      @return the sum of the scores, with the lowest score dropped if 
+      there are at least two scores, or 0 if there are no scores.
+   */
+   public double finalScore() 
+   {
+      if (scoresSize == 0)
+         return 0;
+      else if (scoresSize == 1)
+         return scores[0];
+      else
+         return sum() - minimum();
+   }
+   
+   /**
+    * Gets the size of the scores array; Amount of scores that are stored in the object
+    * @return The size of the scores array
+    */
+   public int getScoreSize() {
+	   return scoresSize;
+   }
+   
+   
+   /**
+    * Returns the toString to see what all of the scores that were saved were
+    * @return All of the scores in the scores array
+    */
+   public String toString() {
+	   String finalString = null;
+	   for(int i = 0; i < scores.length;i++) {
+		   if(i == 0) {
+			   finalString = "The scores are " + scores[i];
+		   } else {
+			   finalString += " " + scores[i];
+		   }
+	   }
+	   
+	   return finalString;
+   }
+}
